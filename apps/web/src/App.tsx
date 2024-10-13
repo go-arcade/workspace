bd5cc9bd77fb5dc0475286bc72731d714fe90d5c
siykt <cnsiykt@163.com>
2024-10-14 00:55:56 +0800

    chore: upd configs

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
+import viteLogo from '../../../../../vite.svg'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -9,16 +9,23 @@ function App() {
   return (
     <>
       <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
+        <a href='https://vitejs.dev' rel='noopener' target='_blank'>
+          <img alt='Vite logo' className='logo' src={viteLogo} />
         </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
+        <a href='https://react.dev' rel='noopener' target='_blank'>
+          <img alt='React logo' className='logo react' src={reactLogo} />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <button
+          onClick={() => {
+            setCount((count) => count + 1)
+          }}
+          type='button'
+        >
+          count is {count}
+        </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
