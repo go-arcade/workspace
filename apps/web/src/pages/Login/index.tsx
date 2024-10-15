ff6347fb0f902bde640835dd8525d6b8fab1ee60
siykt <cnsiykt@163.com>
2024-10-15 15:01:07 +0800

    feat: change login page style

--- apps/web/src/pages/Login/index.tsx
+++ apps/web/src/pages/Login/index.tsx
@@ -9,10 +9,12 @@ const Login: FC<LoginProps> = () => {
     <div className='container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0'>
       <div className='relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex'>
         <div className='absolute inset-0 bg-zinc-900' />
-        <div className='relative z-20 flex items-center text-lg font-medium'>
-          <img alt='logo.png' className='mr-2 h-6 w-6' src={LOGO} />
-          Go Arcade
-        </div>
+        <h1>
+          <a className='relative z-20 flex items-center text-lg font-medium' href='/login'>
+            <img alt='logo.png' className='mr-2 h-6 w-6' src={LOGO} />
+            Go Arcade
+          </a>
+        </h1>
         <div className='relative z-20 mt-auto'>
           <blockquote className='space-y-2'>
             <p className='text-lg'>&ldquo;A modern CI/CD platform.&rdquo;</p>
@@ -21,7 +23,7 @@ const Login: FC<LoginProps> = () => {
         </div>
       </div>
       <div className='lg:p-8'>
-        <div className='mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[380px]'>
+        <div className='mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]'>
           <div className='flex flex-col space-y-2 text-center'>
             <h1 className='text-3xl font-bold'>Login</h1>
             <p className='text-balance text-muted-foreground'>Enter your email below to login to your account</p>
